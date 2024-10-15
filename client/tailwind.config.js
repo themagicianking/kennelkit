91d81031f465504fd589d2d55867a305c1b5389c
Thane Chaves <tmoonder@gmail.com>
2024-10-15 19:12:01 -0400

    Added Material Tailwind to the client.

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+export default module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
