da809c904d6b523ab8994278358d43b49ce52d15
Thane Chaves <tmoonder@gmail.com>
2024-10-15 19:31:49 -0400

    Added dummy data for pet profile component.

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,5 +1,30 @@
+import PetProfile from "./PetProfile";
+
+const PET = {
+  petname: "Watts",
+  species: "dog",
+  breed: "Airedale Terrier",
+  sex: "male",
+  altered: true,
+  birthday: "",
+  weight: 55,
+  physicaldesc: "Black and tan, curly",
+};
+
 export default function Homepage() {
-  return (<>
-  <h1>Welcome to your kennel!</h1>
-  </>)
+  return (
+    <>
+      <h1>Welcome to your kennel!</h1>
+      <PetProfile
+        petname={PET.petname}
+        species={PET.species}
+        breed={PET.breed}
+        sex={PET.sex}
+        altered={PET.altered}
+        birthday={PET.birthday}
+        weight={PET.weight}
+        physicaldesc={PET.physicaldesc}
+      />
+    </>
+  );
 }
---
+++ client/src/components/PetProfile.jsx
@@ -0,0 +1,14 @@
+import { Card } from "@material-tailwind/react";
+
+export default function PetProfile({
+  petname,
+  species,
+  breed,
+  sex,
+  altered,
+  birthday,
+  weight,
+  physicaldesc,
+}) {
+  return <Card></Card>;
+}
