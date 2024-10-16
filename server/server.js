5998051221e71a70d282036169674e6240682ba0
Thane Chaves <tmoonder@gmail.com>
2024-10-16 02:10:21 -0400

    Created endpoint to retrieve pet data.

--- server/server.js
+++ server/server.js
@@ -23,16 +23,71 @@ const Pet = sequelize.define("Pet", {
     type: DataTypes.BOOLEAN,
     allowNull: false,
   },
+  staytype: {
+    type: DataTypes.STRING,
+  },
+  species: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  breed: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sex: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  altered: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  birthday: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  weight: {
+    type: DataTypes.INTEGER,
+  },
+  physicaldesc: {
+    type: DataTypes.STRING,
+  },
+  ownerid: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
 (async () => {
   await sequelize.sync({ force: true });
-  const arcadia = await Pet.create({ petname: "Arcadia", checkedin: true });
+  const arcadia = await Pet.create({
+    petname: "Arcadia",
+    checkedin: true,
+    staytype: "daycare",
+    species: "cat",
+    breed: "Domestic Shorthair",
+    sex: "female",
+    altered: true,
+    birthday: "2023-07-13",
+    weight: 10,
+    physicaldesc: "Shorthaired tuxedo",
+    ownerid: 0,
+  });
   console.log(arcadia.toJSON());
 })();
 
 APP.use(express.json());
 APP.use(cors());
+
+// endpoint to retrieve pet data
+APP.get("/pet", async (req, res) => {
+  const pet = await Pet.findAll({
+    where: {
+      id: req.query.id,
+    },
+  });
+  res.send(pet[0]);
+});
 
 // endpoint to check pet in and out
 APP.put("/pet", async (req, res) => {
