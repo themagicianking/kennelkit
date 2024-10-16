211bd57e7b076757103382d795706c4a0af962e8
Thane Chaves <tmoonder@gmail.com>
2024-10-16 02:23:13 -0400

    Loaded in pet information from server.

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,4 +1,6 @@
 import PetProfile from "./PetProfile";
+import { useState, useEffect } from "react";
+
 
 const OWNER = {
   firstname: "Cara",
@@ -25,22 +27,53 @@ const PET = {
 };
 
 export default function Homepage() {
+  const [pet, setPet] = useState(null);
+
+  async function getPet() {
+    await fetch("http://localhost:5000/pet?id=1")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setPet(data));
+  }
+
+  useEffect(() => {
+    getPet();
+  }, []);
+  
+
   return (
     <>
       <h1>Welcome to your kennel!</h1>
-      <PetProfile
-        petname={PET.petname}
-        species={PET.species}
-        breed={PET.breed}
-        sex={PET.sex}
-        altered={PET.altered}
-        birthday={PET.birthday}
-        weight={PET.weight}
-        physicaldesc={PET.physicaldesc}
-        staytype={PET.staytype}
-        checkedin={PET.checkedin}
-        owner={OWNER}
-      />
+      {pet ? (
+        <PetProfile
+          petname={pet.petname}
+          species={pet.species}
+          breed={pet.breed}
+          sex={pet.sex}
+          altered={pet.altered}
+          birthday={pet.birthday}
+          weight={pet.weight}
+          physicaldesc={pet.physicaldesc}
+          staytype={pet.staytype}
+          checkedin={pet.checkedin}
+          owner={OWNER}
+        />
+      ) : (
+        <PetProfile
+          petname={PET.petname}
+          species={PET.species}
+          breed={PET.breed}
+          sex={PET.sex}
+          altered={PET.altered}
+          birthday={PET.birthday}
+          weight={PET.weight}
+          physicaldesc={PET.physicaldesc}
+          staytype={PET.staytype}
+          checkedin={PET.checkedin}
+          owner={OWNER}
+        />
+      )}
     </>
   );
 }
