b9ec318f4fa630208ed112c7750a54fdbcccf357
Thane Chaves <tmoonder@gmail.com>
2024-10-21 14:40:36 -0400

    Created navbar with links.

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,7 +1,10 @@
+import { Navbar } from "./Navbar";
+
 export function Homepage() {
   return (
-    <>
+    <div className="flex">
+      <Navbar />
       <h1>Welcome to your kennel!</h1>
-    </>
+    </div>
   );
 }
