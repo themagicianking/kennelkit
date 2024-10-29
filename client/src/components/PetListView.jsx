918b6fab0a386b34682c9c970e97cffda121fbc2
Thane Chaves <tmoonder@gmail.com>
2024-10-29 03:01:25 -0400

    Corrected merge error in PetListView.

--- client/src/components/PetListView.jsx
+++ client/src/components/PetListView.jsx
@@ -4,10 +4,10 @@ import { List } from "@material-tailwind/react";
 import { PetItem } from "./PetItem";
 
 export function PetListView({ list, baseURL }) {
-  return list.length > 0 ? list.length > 0 ? (
+  return list.length > 0 ? (
     <List>
       {list.map((listitem) => (
-        <PetItem key={listitem.id} pet={listitem} baseURL={baseURL}  />
+        <PetItem key={listitem.id} pet={listitem} baseURL={baseURL} />
       ))}
     </List>
   ) : (
