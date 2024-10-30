f37ad3299607da410035da1d798ab476a3baf9b9
Thane Chaves <tmoonder@gmail.com>
2024-10-30 18:03:48 -0400

    Wrapped all endpoints in try catch blocks.

--- server/routes.js
+++ server/routes.js
@@ -81,37 +81,53 @@ export const GET = {
   },
 
   catBreeds: async (req, res) => {
-    const catBreeds = await dbhelper.Breed.findAll({
-      where: {
-        species: "cat",
-      },
-    });
-    catBreeds.sort(alphabetizeByName);
-    res.send(catBreeds);
+    try {
+      const catBreeds = await dbhelper.Breed.findAll({
+        where: {
+          species: "cat",
+        },
+      });
+      catBreeds.sort(alphabetizeByName);
+      res.send(catBreeds);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   dogBreeds: async (req, res) => {
-    const dogBreeds = await dbhelper.Breed.findAll({
-      where: {
-        species: "dog",
-      },
-    });
-    dogBreeds.sort(alphabetizeByName);
-    res.send(dogBreeds);
+    try {
+      const dogBreeds = await dbhelper.Breed.findAll({
+        where: {
+          species: "dog",
+        },
+      });
+      dogBreeds.sort(alphabetizeByName);
+      res.send(dogBreeds);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   allPets: async (req, res) => {
-    const petlist = await dbhelper.Pet.findAll();
-    res.send(petlist);
+    try {
+      const petlist = await dbhelper.Pet.findAll();
+      res.send(petlist);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   allCheckedInPets: async (req, res) => {
-    const petlist = await dbhelper.Pet.findAll({
-      where: {
-        checkedin: true,
-      },
-    });
-    res.send(petlist);
+    try {
+      const petlist = await dbhelper.Pet.findAll({
+        where: {
+          checkedin: true,
+        },
+      });
+      res.send(petlist);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   petById: async (req, res) => {
@@ -128,99 +144,123 @@ export const GET = {
   },
 
   petsByOwner: async (req, res) => {
-    const petlist = await dbhelper.Pet.findAll({
-      where: {
-        ownerid: req.query.id,
-      },
-    });
-    res.send(petlist);
+    try {
+      const petlist = await dbhelper.Pet.findAll({
+        where: {
+          ownerid: req.query.id,
+        },
+      });
+      res.send(petlist);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 };
 
 export const POST = {
   owner: async (req, res) => {
-    await dbhelper.db.sync();
-    const newOwner = await dbhelper.Owner.create({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      phone: req.body.phone,
-      email: req.body.email,
-    });
+    try {
+      await dbhelper.db.sync();
+      const newOwner = await dbhelper.Owner.create({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        phone: req.body.phone,
+        email: req.body.email,
+      });
 
-    console.log(newOwner.toJSON());
-    res.send(newOwner);
+      console.log(newOwner.toJSON());
+      res.send(newOwner);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   pet: async (req, res) => {
-    await dbhelper.db.sync();
-    const newPet = await dbhelper.Pet.create({
-      petname: req.body.petname,
-      checkedin: false,
-      staytype: null,
-      species: req.body.species,
-      breed: req.body.breed,
-      sex: req.body.sex,
-      altered: req.body.altered,
-      birthday: req.body.birthday,
-      weight: req.body.weight,
-      physicaldesc: req.body.physicaldesc,
-      ownerid: req.body.ownerid,
-    });
+    try {
+      await dbhelper.db.sync();
+      const newPet = await dbhelper.Pet.create({
+        petname: req.body.petname,
+        checkedin: false,
+        staytype: null,
+        species: req.body.species,
+        breed: req.body.breed,
+        sex: req.body.sex,
+        altered: req.body.altered,
+        birthday: req.body.birthday,
+        weight: req.body.weight,
+        physicaldesc: req.body.physicaldesc,
+        ownerid: req.body.ownerid,
+      });
 
-    console.log(newPet.toJSON());
-    res.send(newPet);
+      console.log(newPet.toJSON());
+      res.send(newPet);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 };
 
 export const PUT = {
   owner: async (req, res) => {
-    await dbhelper.Owner.update(
-      {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        phone: req.body.phone,
-        email: req.body.email,
-      },
-      { where: { id: req.body.id } }
-    );
-    const editedOwners = await dbhelper.Owner.findAll({
-      where: {
-        id: req.body.id,
-      },
-    });
-    res.status(200).send(editedOwners[0]);
+    try {
+      await dbhelper.Owner.update(
+        {
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          phone: req.body.phone,
+          email: req.body.email,
+        },
+        { where: { id: req.body.id } }
+      );
+      const editedOwners = await dbhelper.Owner.findAll({
+        where: {
+          id: req.body.id,
+        },
+      });
+      res.status(200).send(editedOwners[0]);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   pet: async (req, res) => {
-    await dbhelper.Pet.update(
-      {
-        sex: req.body.sex,
-        altered: req.body.altered,
-        species: req.body.species,
-        breed: req.body.breed,
-        weight: req.body.weight,
-        physicaldesc: req.body.physicaldesc,
-      },
-      { where: { id: req.body.id } }
-    );
-    const editedPets = await dbhelper.Pet.findAll({
-      where: { id: req.body.id },
-    });
-    res.status(200).send(editedPets[0]);
+    try {
+      await dbhelper.Pet.update(
+        {
+          sex: req.body.sex,
+          altered: req.body.altered,
+          species: req.body.species,
+          breed: req.body.breed,
+          weight: req.body.weight,
+          physicaldesc: req.body.physicaldesc,
+        },
+        { where: { id: req.body.id } }
+      );
+      const editedPets = await dbhelper.Pet.findAll({
+        where: { id: req.body.id },
+      });
+      res.status(200).send(editedPets[0]);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 
   checkin: async (req, res) => {
-    await dbhelper.Pet.update(
-      { checkedin: req.body.checkedin },
-      {
-        where: { id: req.body.id },
-      }
-    );
-    const editedPets = await dbhelper.Pet.findAll({
-      where: {
-        id: req.body.id,
-      },
-    });
-    res.status(200).send(editedPets[0].checkedin);
+    try {
+      await dbhelper.Pet.update(
+        { checkedin: req.body.checkedin },
+        {
+          where: { id: req.body.id },
+        }
+      );
+      const editedPets = await dbhelper.Pet.findAll({
+        where: {
+          id: req.body.id,
+        },
+      });
+      res.status(200).send(editedPets[0].checkedin);
+    } catch (e) {
+      res.status(404).send(e);
+    }
   },
 };
