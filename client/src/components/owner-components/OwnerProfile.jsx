6c9aebda6ea3412cc865278c5aa0795a704b3f2f
Thane Chaves <tmoonder@gmail.com>
2024-10-30 13:37:47 -0400

    Added error message to OwnerProfile.

--- client/src/components/owner-components/OwnerProfile.jsx
+++ client/src/components/owner-components/OwnerProfile.jsx
@@ -37,7 +37,7 @@ export function OwnerProfile() {
     } catch (e) {
       setOwner(null);
       setLoading(false);
-      console.log("Could not fetch owner.");
+      console.log("Could not fetch owner. The following error occurred:", e);
     }
   }
 
