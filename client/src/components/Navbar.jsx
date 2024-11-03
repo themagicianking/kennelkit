ce334f9b3237c19590ca973189b873d8b8bf3b11
Thane Chaves <tmoonder@gmail.com>
2024-11-03 01:48:08 -0500

    Added rounded edge to navbar to make it look like a folder.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,48 +5,53 @@ import { List, ListItem, ListItemPrefix } from "@material-tailwind/react";
 
 export function Navbar({ selected }) {
   return (
-    <List className="navbar">
-      <ListItem className="main-tabs">
-      <a href="/">
-        <ListItem
-          className={"navtab " + (selected == 0 ? "selected-tab" : "hometab")}
-        >
-          <ListItemPrefix>
-            <i className="fas fa-house" />
-          </ListItemPrefix>
-          Home
+    <div className="navbar-container">
+      <List className="navbar">
+        <ListItem className="main-tabs">
+          <a href="/">
+            <ListItem
+              className={
+                "navtab " + (selected == 0 ? "selected-tab" : "hometab")
+              }
+            >
+              <ListItemPrefix>
+                <i className="fas fa-house" />
+              </ListItemPrefix>
+              Home
+            </ListItem>
+          </a>
+          <a href="/pets/checkedin">
+            <ListItem className="navtab">
+              <ListItemPrefix>
+                <i className="fas fa-sun" />
+              </ListItemPrefix>
+              Checked In
+            </ListItem>
+          </a>
+          <a href="/pets">
+            <ListItem className="navtab">
+              <ListItemPrefix>
+                <i className="fas fa-paw" />
+              </ListItemPrefix>
+              All Pets
+            </ListItem>
+          </a>
+          <a href="/owners">
+            <ListItem className="navtab">
+              <ListItemPrefix>
+                <i className="fas fa-user" />
+              </ListItemPrefix>
+              All Owners
+            </ListItem>
+          </a>
         </ListItem>
-      </a>
-      <a href="/pets/checkedin">
-        <ListItem className="navtab">
-          <ListItemPrefix>
-            <i className="fas fa-sun" />
-          </ListItemPrefix>
-          Checked In
+        <ListItem className="form-tabs">
+          <CreatePetForm />
+          <CreateOwnerForm />
         </ListItem>
-      </a>
-      <a href="/pets">
-        <ListItem className="navtab">
-          <ListItemPrefix>
-            <i className="fas fa-paw" />
-          </ListItemPrefix>
-          All Pets
-        </ListItem>
-      </a>
-      <a href="/owners">
-        <ListItem className="navtab">
-          <ListItemPrefix>
-            <i className="fas fa-user" />
-          </ListItemPrefix>
-          All Owners
-        </ListItem>
-      </a>
-      </ListItem>
-      <ListItem className="form-tabs">
-        <CreatePetForm />
-        <CreateOwnerForm />
-      </ListItem>
-    </List>
+      </List>
+      <div className="folder-corner"></div>
+    </div>
   );
 }
 
